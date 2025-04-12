6e12bb7cd896418a577948992af2fd17116b0528
MomDra <yem9087@gmail.com>
2025-04-12 17:50:13 +0900

    3개의 Rain을 위한 SpawnManager 제작 중

--- Assets/Scripts/ZombieSpawner.cs
+++ Assets/Scripts/ZombieSpawner.cs
@@ -1,9 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ZombieSpawner : MonoBehaviour
 {
+    [SerializeField]
+    private Transform[] rains;
+
     [SerializeField]
     private GameObject normalZombiePrefab;
 
@@ -12,6 +16,23 @@ public class ZombieSpawner : MonoBehaviour
 
     [SerializeField]
     private Vector3 pos;
+
+    [SerializeField]
+    [Range(1f, 5f)]
+    private float minSpawnCool;
+
+    [SerializeField]
+    [Range(6f, 10f)]
+    private float maxSpawnCool;
+
+    [SerializeField]
+    private int maxZombieCountPerRain;
+
+    private void Start()
+    {
+        foreach (Transform rain in rains)
+            StartCoroutine(SpawnZombie(rain));
+    }
 
     private void Update()
     {
@@ -22,6 +43,40 @@ public class ZombieSpawner : MonoBehaviour
         else if(Input.GetKeyDown(KeyCode.S))
         {
             Instantiate(slowZombiePrefab, pos, Quaternion.identity);
+        }
+    }
+
+    private IEnumerator SpawnZombie(Transform rain)
+    {
+        int spawnCount = 0;
+
+        while (true)
+        {
+            WaitForSeconds wait = new WaitForSeconds(Random.Range(minSpawnCool, maxSpawnCool));
+            yield return wait;
+
+            int val = Random.Range(0, 2);
+
+            GameObject spawnPrefab = default;
+
+            switch(val)
+            {
+                case 0:
+                    // normal Zombie
+                    spawnPrefab = normalZombiePrefab;
+                    break;
+
+                case 1:
+                    // slow Zombie
+                    spawnPrefab = slowZombiePrefab;
+                    break;
+            }
+
+            Instantiate(spawnPrefab, pos, Quaternion.identity, rain);
+
+            ++spawnCount;
+
+            if (spawnCount == maxZombieCountPerRain) break;
         }
     }
 }
